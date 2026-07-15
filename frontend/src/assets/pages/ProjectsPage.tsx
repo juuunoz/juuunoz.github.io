6798d094c1616fd5525ca6769ea6f20d1ab312f2
juuunoz <junoazhang@gmail.com>
2026-07-15 15:54:08 -0400

    add scrolling for projects page

--- frontend/src/assets/pages/ProjectsPage.tsx
+++ frontend/src/assets/pages/ProjectsPage.tsx
@@ -1,55 +1,63 @@
 import { BodyEntry } from "../BodyEntry"
 import Markdown from "react-markdown"
+import { useRef } from "react"
 
 import project1 from '../../markdown/project1.md?raw'
 
 const ProjectEntry = ({children, title} : {children: React.ReactNode, title: string}) => {
     return (
       <div className="mb-20">
-        <p className="text-2xl">{title}    v</p>
+        <p className="text-2xl mb-5">{title}</p>
         {children}
       </div>
     )
   }
 
 export const ProjectsPage = () => {
-  const ProjectLabel = ({label} : {label: string}) => {
-  
-          return (
-              <li 
-                  className="break-words hover:outline outline-1 p-1">
-                  {label}
-              </li>)
-      }
+  const projectRefs = useRef<HTMLDivElement[]>([]);
+  const projectFiles = [project1, project1, project1];
+  const projectTitles: string[] = [
+    'reflections on making junozhang.com ', 
+    'web-based party game (IN DEVELOPMENT)' 
+    ]
 
-    return (
-    <div className="pt-[5vh] flex flex-col md:flex-row">
-        <div className="w-full md:w-1/3 md:m-5 mt-6 ">
-            <BodyEntry>
-                <ul>
-                <ProjectLabel label="project 1"/>
-                <ProjectLabel label="project 2"/>
-                <ProjectLabel label="project 3"/>
-                </ul>
-            </BodyEntry>
+  const ProjectLabel = ({label, index} : {label: string, index: number}) => {
+      return (
+          <li 
+              onClick={() => {projectRefs.current[index].scrollIntoView({behavior: "smooth"})}}
+              className="break-words hover:outline outline-1 p-1">
+              {label}
+          </li>)
+  }
+      
+  return (
+  <div className="pt-[5vh] flex flex-col md:flex-row">
+      <div className="w-full md:w-1/3 md:m-5 mt-6 ">
+          <BodyEntry>
+              <ul>
+                {projectTitles.map((item, index) => (
+                  <ProjectLabel label={item} index={index}/>
+                ))
+                }
+              </ul>
+          </BodyEntry>
 
-        </div>
-        <div className="w-full md:w-2/3 md:m-5 max-h-[650px] overflow-scroll">
-            <BodyEntry>
-              <ProjectEntry
-                title="project 1">
-                  <Markdown>{project1}</Markdown>
-              </ProjectEntry>
-              <ProjectEntry
-                title="project 2">
-                  <Markdown>{project1}</Markdown>
-              </ProjectEntry>
-              <ProjectEntry
-                title="project 3">
-                  <Markdown>{project1}</Markdown>
-              </ProjectEntry>
-            </BodyEntry>
-        </div>
-    </div>
-    )
+      </div>
+      <div className="w-full md:w-2/3 md:m-5 max-h-[650px] overflow-scroll">
+          <BodyEntry>
+            {projectFiles.map((item, index) => (
+              <div 
+                key={item} 
+                ref={(node) => { node ? projectRefs.current[index] = node: null; }}
+              >
+                <ProjectEntry
+                  title={projectTitles[index]}>
+                  <Markdown>{projectFiles[index]}</Markdown>
+                </ProjectEntry>
+              </div>
+            ))}
+          </BodyEntry>
+      </div>
+  </div>
+  )
 }
