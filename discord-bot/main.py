e6b48ca0eccc222024b50adaace524e90836fadd
Juno Angela Zhang <53055796+juuunoz@users.noreply.github.com>
2026-07-20 17:00:42 -0400

    temporarily remove message editing

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -58,22 +58,22 @@ class MyClient(discord.Client):
             print(f"Failed with status code: {response.status_code}")
             print(f"Message: {response.reason}")
 
-    async def on_message_edit(self, before, after):
-        content, topics = extract_hashtags_regex(after.content)
+#    async def on_message_edit(self, before, after):
+#        content, topics = extract_hashtags_regex(after.content)
         
-        payload = {
-            "topics": topics,
-            "content": content
-        }
+#        payload = {
+#            "topics": topics,
+#            "content": content
+#        }
 
-        endpoint = url  + "/api" + "/notes/" + str(after.id)
-        response = requests.patch(endpoint, json=payload)
+#        endpoint = url  + "/api" + "/notes/" + str(after.id)
+#        response = requests.patch(endpoint, json=payload)
 
-        if response.status_code == 200 or response.status_code == 201:
-            print("Success!")
-            print(response.json())
-        else:
-            print(f"Failed with status code: {response.status_code}")
+#        if response.status_code == 200 or response.status_code == 201:
+#            print("Success!")
+#            print(response.json())
+#        else:
+#            print(f"Failed with status code: {response.status_code}")
 
 
 intents = discord.Intents.default()
